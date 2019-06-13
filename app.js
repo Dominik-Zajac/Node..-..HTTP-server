9ae5f865dcdc3eb2a0efdb0c11b5b0994ecac22d
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-13 15:18:39 +0200

    Add index.html and users.html

--- app.js
+++ app.js
@@ -3,22 +3,23 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8'
-        })
-        res.end('<h1>Strona glowna</h1>')
-    } else if (req.url === '/users') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8'
-        })
-        res.end('<h1>Strona uzytkownikow</h1>');
-    } else {
-        res.writeHead(404, {
-            'Content-Type': 'text/html; charset=utf-8'
-        })
-        res.end('<h1>Brak strony o podanym adresie</h1>');
+    res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+    })
+    switch (req.url) {
+        case '/':
+            res.end('<h1>Strona glowna</h1>');
+            break;
+        case '/users':
+            res.end('<h1>Strona uzytkownikow</h1>');
+            break;
+        case '/api/users':
+            res.end('<h1>API</h1>');
+            break;
+        default:
+            res.end('<h1>Strona nie istnieje</h1>')
     }
+
 }).listen(port, '127.0.0.1', () => {
     console.log(`Nasluchuje na porcie ${port}`);
 })
